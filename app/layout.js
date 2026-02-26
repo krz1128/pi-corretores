18eef78640146f54cfd3ceb9f85c9cb023d5d76b
krz1128 <krisleymiranda2020@gmail.com>
2026-02-25 22:13:06 -0300

    Projeto base

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,3 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
