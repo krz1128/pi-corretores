142f5ad33d6643e360bb8cc7d24192e8fc633b97
raiannycardoso <raiannydondacardoso143@gmail.com>
2026-02-26 21:34:13 -0300

    layout corrigido

--- app/cadastro_usuario/page.js
+++ app/cadastro_usuario/page.js
@@ -1,7 +1,77 @@
-export default function CadastroUsuario(){
-    return(
+export default function CadastroUsuario() {
+    return (
         <div>
-            <h1>Página de cadastro de usuários (Raianny) </h1>
+            <div class="menuSuperior">
+
+                <div>
+
+                    <div class="col-8 bg-dark menuLateral">
+
+                        <div class="text-center mt-5">
+                            <h1 class="fs-1"> Cadastre-se </h1>
+                        </div>
+
+
+                        <form onsubmit="salvar(event)">
+
+
+
+                            <label>
+                                Nome completo:
+                                <br />
+                                <input required class="inputNome" />
+                            </label>
+
+                            <br /><br />
+
+                            <label>
+                                Telefone:
+                                <br />
+                                <input required class="inputTelefone" type="tel" />
+                            </label>
+
+                            <br /><br />
+
+                            <label>
+                                Crie seu login:
+                                <br />
+                                <input required class="inputLogin" />
+                            </label>
+
+                            <br /><br />
+
+                            <label>
+                                Crie sua senha:
+                                <br />
+                                <input required class="inputSenha" type="password" />
+                            </label>
+
+                            <p>Em qual perfil você se encaixa?</p>
+
+                            <select>
+                                <option value="someOption">Escolha uma opção</option>
+                                <option value="someOption">Corretor</option>
+                                <option value="otherOption">Imobiliária</option>
+                            </select>
+
+
+                            <br /><br />
+
+                            <button type="salvar"> Criar Login</button>
+
+
+                        </form>
+                    </div>
+
+                    <hr />
+
+
+                </div>
+
+            </div>
+
         </div>
+
+
     )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Geist, Geist_Mono } from "next/font/google"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Script from 'next/script';
 
