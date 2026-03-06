fa9a04628bbdf9b1a4405429abd71f333cf0f54b
lucascgouvea <lucasc.barbosa@hotmail.com>
2026-03-05 21:06:25 -0300

    cadastro empreendimento modal funcionando

--- app/cadastro_empreendimento/page.js
+++ app/cadastro_empreendimento/page.js
@@ -1,6 +1,114 @@
+'use client'
 import "./cadastro_empreendimento.css"
+import { useState } from "react";
 export default function CadastroEmpreendimento() {
+
+    const [construtora, alteraConstrutora] = useState("")
+    const [tipoimovel, alteraTipoimovel] = useState("")
+    const [pagamento, alteraPagamento] = useState("")
+    const [prazo, alteraPrazo] = useState("")
+    const [unidades, alteraUnidades] = useState("")
+    const [condominio, alteraCondominio] = useState("")
+
+    const [empreendimentos, alteraCadastroEmpreendimento] = useState([
+  {
+    construtora: "Tecnisa",
+    tipoImovel: "Apartamento (3 Quartos)",
+    pagamento: "Financiamento Bancário / 30% Entrada",
+    prazoEntrega: "Dezembro 2026",
+    unidadesDisponiveis: 12,
+    condominio: "R$ 850,00"
+  },
+  {
+    construtora: "MRV",
+    tipoImovel: "Studio",
+    pagamento: "Minha Casa Minha Vida / Parcelamento em 60x",
+    prazoEntrega: "Pronto para morar",
+    unidadesDisponiveis: 4,
+    condominio: "R$ 280,00"
+  },
+  {
+    construtora: "Cyrela",
+    tipoImovel: "Cobertura Duplex",
+    pagamento: "A vista ou Fluxo Direto",
+    prazoEntrega: "Junho 2027",
+    unidadesDisponiveis: 2,
+    condominio: "R$ 2.400,00"
+  },
+  {
+    construtora: "Even",
+    tipoImovel: "Apartamento (2 Quartos)",
+    pagamento: "Financiamento SAC",
+    prazoEntrega: "Março 2026",
+    unidadesDisponiveis: 25,
+    condominio: "R$ 620,00"
+  },
+  {
+    construtora: "Gafisa",
+    tipoImovel: "Garden",
+    pagamento: "30% durante obra / 70% na entrega",
+    prazoEntrega: "Outubro 2025",
+    unidadesDisponiveis: 5,
+    condominio: "R$ 1.100,00"
+  },
+  {
+    construtora: "Moura Dubeux",
+    tipoImovel: "Empresarial (Sala)",
+    pagamento: "Investimento Direto",
+    prazoEntrega: "Janeiro 2028",
+    unidadesDisponiveis: 18,
+    condominio: "R$ 450,00"
+  },
+  {
+    construtora: "Tenda",
+    tipoImovel: "Apartamento (1 Quarto)",
+    pagamento: "Subsídio do Governo / FGTS",
+    prazoEntrega: "Julho 2026",
+    unidadesDisponiveis: 40,
+    condominio: "R$ 210,00"
+  },
+  {
+    construtora: "Eztec",
+    tipoImovel: "Apartamento (4 Suítes)",
+    pagamento: "Plano Empresário",
+    prazoEntrega: "Agosto 2027",
+    unidadesDisponiveis: 7,
+    condominio: "R$ 3.200,00"
+  },
+  {
+    construtora: "Direcional",
+    tipoImovel: "Casa em Condomínio",
+    pagamento: "Crédito Imobiliário Caixa",
+    prazoEntrega: "Maio 2026",
+    unidadesDisponiveis: 15,
+    condominio: "R$ 390,00"
+  },
+  {
+    construtora: "Mitre",
+    tipoImovel: "Loft Moderno",
+    pagamento: "Entrada facilitada em 36x",
+    prazoEntrega: "Novembro 2026",
+    unidadesDisponiveis: 9,
+    condominio: "R$ 550,00"
+  }
+]);
+
+function salvar(e) {
+    e.preventDefault()
+    const objeto = {
+      construtora: construtora,
+      tipoImovel: tipoimovel,
+      pagamento: pagamento,
+      prazo: prazo,
+      unidades: unidades,
+      condominio: condominio
+
+    }
+    alteraCadastroEmpreendimento(empreendimentos.concat(objeto))
+}
+    
     return (
+
         <div>
             <h1>Cadastro de empreendimentos</h1>
             <br />
@@ -42,7 +150,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="recipient-name" className="col-form-label">
                                         Construtora:
                                     </label>
-                                    <input
+                                    <input  onChange={e => alteraConstrutora(e.target.value)}
                                         type="text"
                                         className="form-control"
                                         id="recipient-name"
@@ -53,7 +161,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="recipient-name" className="col-form-label">
                                         Tipo de imóvel:
                                     </label>
-                                    <input
+                                    <input onChange={e => alteraTipoimovel(e.target.value)}
                                         type="text"
                                         className="form-control"
                                         id="recipient-name"
@@ -64,7 +172,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="recipient-name" className="col-form-label">
                                         Forma de pagamento:
                                     </label>
-                                    <input
+                                    <input onChange={e => alteraPagamento(e.target.value)}
                                         type="text"
                                         className="form-control"
                                         id="recipient-name"
@@ -75,7 +183,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="recipient-name" className="col-form-label">
                                         Prazo de entrega:
                                     </label>
-                                    <input
+                                    <input onChange={e => alteraPrazo(e.target.value)}
                                         type="text"
                                         className="form-control"
                                         id="recipient-name"
@@ -86,7 +194,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="recipient-name" className="col-form-label">
                                         Unidades disponíveis:
                                     </label>
-                                    <input
+                                    <input onChange={e => alteraUnidades(e.target.value)}
                                         type="text"
                                         className="form-control"
                                         id="recipient-name"
@@ -97,7 +205,7 @@ export default function CadastroEmpreendimento() {
                                     <label htmlFor="message-text" className="col-form-label">
                                         Condomínio:
                                     </label>
-                                    <textarea
+                                    <textarea onChange={e => alteraCondominio(e.target.value)}
                                         className="form-control"
                                         id="message-text"
                                     ></textarea>
@@ -113,7 +221,7 @@ export default function CadastroEmpreendimento() {
                             >
                                 Cancelar
                             </button>
-                            <button type="button" className="btn btn-primary">
+                            <button onClick={salvar} type="button" className="btn btn-primary">
                                 Cadastrar
                             </button>
                         </div>
@@ -137,7 +245,18 @@ export default function CadastroEmpreendimento() {
                 </thead>
 
                 <tbody>
-                    <tr></tr>
+                  {
+            empreendimentos.map(
+              item => <tr>
+                <th scope="row">{item.construtora}</th>
+                <td>{item.tipoImovel}</td>
+                <td>{item.pagamento}</td>
+                <td>{item.unidadesDisponiveis}</td>
+                <td>{item.prazoEntrega}</td>
+                <td>{item.condominio}</td>
+              </tr>
+            )
+          }
                 </tbody>
             </table>
         </div>
