8e41c968884d846a5192b4412b7b46ef63297ab6
raiannycardoso <raiannydondacardoso143@gmail.com>
2026-03-13 21:49:30 -0300

    Cadastro Corretores funcionando com o banco de dados

--- app/corretores/page.js
+++ app/corretores/page.js
@@ -1,10 +1,10 @@
 'use client'
 import Link from "next/link"
 import "./corretores.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { createClient, FunctionRegion } from '@supabase/supabase-js'
-const supabase = createClient('https://ksrbtutpfybyalxyijxd.supabase.co', 'sb_publishable_teTpea3gZLy8U1iDzWuR4Q_z4Mu_9Aa')
+const supabase = createClient('https://ogybpinvvqkfjvotqzcf.supabase.co', 'sb_publishable_SOLcXSeorAHNpnq8o04xkw_IllVGRXg')
 
 
 
@@ -12,119 +12,144 @@ function Corretores() {
 
     const [nome, alteraNome] = useState("")
     const [email, alteraEmail] = useState("")
-    const [telefone, alteraTelefone] = useState("")
+    const [senha, alteraSenha] = useState("")
 
-    const [listaCorretores, alteraListaCorretores] = useState([
-        {
-            nome: "João Silva",
-            email: "joao.silva@imobiliaria.com",
-            telefone: "(11) 98765-4321"
-        },
-        {
-            nome: "Maria Oliveira",
-            email: "maria.oliveira@imobiliaria.com",
-            telefone: "(21) 99876-5432"
-        },
-        {
-            nome: "Carlos Souza",
-            email: "carlos.souza@imobiliaria.com",
-            telefone: "(31) 91234-5678"
-        },
-        {
-            nome: "Ana Costa",
-            email: "ana.costa@imobiliaria.com",
-            telefone: "(41) 97654-3210"
-        }
-    ]);
+    async function buscar() {
+        const { data, error } = await supabase.from('usuarios').select()
 
-    function salvar(e) {
-        e.preventDefault()
+        console.log(data)
+        alteraListaCorretores(data)
+    }
+
+
+    const [listaCorretores, alteraListaCorretores] = useState([]);
+
+
+
+    async function salvar(e) {
 
         const objeto = {
             nome: nome,
             email: email,
-            telefone: telefone,
+            senha: senha
+        }
+        
+        
+        if (objeto.email.length < 3) {
+            alert("Usuário muito curto")
+            return
+        }
+        
+        if (objeto.email.length > 100) {
+            alert("Usuário muito longo")
+            return
+        }
+        
+        if (objeto.email.includes("@") == false || objeto.email.includes(".") == false) {
+            alert("O e-mail deve ter um @ ou um .")
+            return
+        }
+        
+        
+        
+        const { error } = await supabase.from('usuarios').insert(objeto)
+        console.log(error)
+        
+        if (error == null) {
+            alert("Usuário cadastrado com sucesso!")
+            alteraNome("")
+            alteraEmail("")
+            alteraSenha("")
+            
+        } else {
+            alert("Dados inválidos, verifique os campos e tente novamente...")
         }
 
-        alteraListaCorretores(listaCorretores.concat(objeto))
     }
 
-
-    return (
-        // div principal
-        <div>
+    
 
 
-            <div class="titulo"></div>
-            <h1>Página de Corretores (Raianny) </h1>
+
+useEffect(() => {
+    buscar()
+}, [])
 
 
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Adicionar Corretor</button>
 
-            {/* div modal */}
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h2 class="modal-title fs-5" id="exampleModalLabel">Novo Corretor</h2>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form>
-                                <div class="mb-3">
-                                    <label for="recipient-name" class="col-form-label">Nome:</label>
-                                    <input type="text" class="form-control" id="recipient-name" onChange={e => alteraNome(e.target.value)} />
-                                </div>
-                                <div class="mb-3">
-                                    <label for="e-mail" class="col-form-label">e-mail: </label>
-                                    <textarea class="form-control" id="message-text" onChange={e => alteraEmail(e.target.value)}></textarea>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="telefone" class="col-form-label">Telefone: </label>
-                                    <textarea class="form-control" id="message-text" onChange={e => alteraTelefone(e.target.value)}></textarea>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button onClick={salvar} class="btn btn-primary">Salvar</button>
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+return (
+    // div principal
+    <div>
 
-                            modal.close();
-                             
-                        </div>
+
+        <div class="titulo"></div>
+        <h1>Página de Corretores (Raianny) </h1>
+
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Adicionar Corretor</button>
+
+        {/* div modal */}
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="modal-title fs-5" id="exampleModalLabel">Novo Corretor</h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="recipient-name" class="col-form-label">Nome:</label>
+                                <input value={(nome)} type="text" class="form-control" id="recipient-name" onChange={e => alteraNome(e.target.value)} />
+                            </div>
+                            <div class="mb-3">
+                                <label for="e-mail" class="col-form-label">e-mail: </label>
+                                <textarea value={(email)} class="form-control" id="message-text" onChange={e => alteraEmail(e.target.value)}></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label   class="col-form-label">Senha: </label>
+                                <textarea value={(senha)} class="form-control" id="message-text" onChange={e => alteraSenha(e.target.value)}></textarea>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button onClick={salvar} class="btn btn-primary">Salvar</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+
                     </div>
                 </div>
             </div>
-
-            {/* tabela informações corretores */}
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col">Telefone</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {
-                        listaCorretores.map(,
-                            
-                            item => <tr>
-                                <th scope="row">{item.nome}</th>
-                                <td>{item.email}</td>
-                                <td>{item.telefone}</td>
-                            </tr>
-                        )
-                    }
-
-                </tbody>
-            </table>
-
-
         </div>
 
-    )
+        {/* tabela informações corretores */}
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Senha</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                {
+                    listaCorretores.map(
+
+                        item => <tr>
+                            <th scope="row">{item.nome}</th>
+                            <td>{item.email}</td>
+                            <td>{item.senha}</td>
+                        </tr>
+                    )
+                }
+
+            </tbody>
+        </table>
+
+
+    </div>
+
+)
 }
 
 export default Corretores
