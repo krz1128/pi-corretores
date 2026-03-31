57714a6892477cf3aa1a524c5ccab6116f13a5b4
raiannycardoso <raiannydondacardoso143@gmail.com>
2026-03-31 19:37:35 -0300

    página dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -62,7 +62,7 @@ export default function Dashboard() {
 
                         <div className="card cartao2 " style={{ width: 300 }}>
                             <div className="card-body">
-                                <h5 className="card-title"><strong>30 dias sem contato</strong></h5>
+                                <h5 className="card-title"><strong>Imóveis vendidos</strong></h5>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                                
@@ -77,7 +77,7 @@ export default function Dashboard() {
 
                         <div className="card cartao2 " style={{ width: 300 }}>
                             <div className="card-body">
-                                <h5 className="card-title"><strong>60 dias sem contato</strong></h5>
+                                <h5 className="card-title"><strong>Imóveis alugados</strong></h5>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                                 
@@ -91,7 +91,7 @@ export default function Dashboard() {
 
                         <div className="card cartao2 " style={{ width: 300 }}>
                             <div className="card-body">
-                                <h5 className="card-title"><strong>90 dias sem contato</strong></h5>
+                                <h5 className="card-title"><strong>Imóveis dispniveis </strong></h5>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                                 
