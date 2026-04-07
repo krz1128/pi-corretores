494c7c444458aa78dcab6a2dd1d207fc7e2e24cd
raiannycardoso <raiannydondacardoso143@gmail.com>
2026-04-07 20:26:58 -0300

    menu lateral arrumado

--- app/components/MenuLateral.js
+++ app/components/MenuLateral.js
@@ -1,44 +1,35 @@
 import Link from "next/link";
-import "./MenuLateral.css"
+import "./MenuLateral.css";
 
-export default function Menulateral() {
+export default function MenuLateral() {
     return (
-        <div className="">
+        <div className="menulateral d-flex flex-column justify-content-between">
 
-
-            <div className="text-center mt-5">
+            <div className="menuLinks mt-4">
+                <h3 className="text-center mb-4">Dashboard</h3>
+                <div className="list-group list-group-flush fs-5">
+                    <Link href="dashboard" className="list-group-item list-group-item-action">Início</Link>
+                    <Link href="lista_diaria" className="list-group-item list-group-item-action">Lista diária</Link>
+                    <Link href="corretores" className="list-group-item list-group-item-action">Corretores</Link>
+                    <Link href="clientes" className="list-group-item list-group-item-action">Clientes</Link>
+                    <Link href="cadastro_empreendimento" className="list-group-item list-group-item-action">Empreendimentos</Link>
+                    <Link href="imoveis" className="list-group-item list-group-item-action">Imóveis</Link>
+                </div>
             </div>
 
-            <div className="botaoinicio list-group list-group-flush fs-4">
-                <Link href="dashboard" className="list-group-item list-group-item-action botaoinicio" aria-current="true">Início</Link>
-                <Link href="lista_diaria" className="list-group-item list-group-item-action">Lista diária</Link>
-                <Link href="corretores" className="list-group-item list-group-item-action active">Corretores</Link>
-                <Link href="clientes" className="list-group-item list-group-item-action">Clientes</Link>
-                <Link href="cadastro_empreendimento" className="list-group-item list-group-item-action">Empreendimentos</Link>
-                <Link href="imoveis" className="list-group-item list-group-item-action">Imóveis</Link>
-
-            </div>
-
-            <div className="text-center menuLateralPerfil">
-                <img className="me-4" src="https://placehold.co/50" />
-
+            <div className="menuPerfil text-center mb-4">
+                <img className="perfilFoto mb-2" src="https://placehold.co/50" alt="Perfil" />
                 <div className="btn-group dropend">
-                    <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                    <button type="button" className="btn btn-outline-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         Perfil
                     </button>
                     <ul className="dropdown-menu">
-                        <li><a className="dropdown-item active" href="#">Editar</a></li>
+                        <li><a className="dropdown-item" href="#">Editar</a></li>
                         <li><a className="dropdown-item" href="#">Sair</a></li>
                     </ul>
                 </div>
-
             </div>
 
-
-
-
         </div>
-
-    )
+    );
 }
